e52e58f6bebc7ea8bec2cfe107ca2ce88e3d0f7e
mzoric33 <zoricmarin4@gmail.com>
2023-01-06 14:04:03 +0100

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHVGz70ua88swrP80yf_wFtau1ITmMnoQ",
+  authDomain: "netflix-clone-50c03.firebaseapp.com",
+  projectId: "netflix-clone-50c03",
+  storageBucket: "netflix-clone-50c03.appspot.com",
+  messagingSenderId: "471196221048",
+  appId: "1:471196221048:web:e722ce69a936bc1610b78d",
+};
