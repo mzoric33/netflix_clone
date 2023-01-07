9662b6f1f0bf510c8aff9c660a268416c2dea855
mzoric33 <zoricmarin4@gmail.com>
2023-01-07 16:38:38 +0100

    sign in screen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,7 +2,32 @@ import React from "react";
 import "./SignInScreen.css";
 
 const SignInScreen = () => {
-  return <div className="signInScreen"></div>;
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signInScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signInScreen__gray">New to Netflix?</span>{" "}
+          <span className="signInScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
 };
 
 export default SignInScreen;
